533feea68504ed72b67a1378788c63d3f18552a8
Анастасия Кузнецова <kuznetsova.anastasiia@phystech.edu>
2022-10-26 17:28:39 +0300

    8 task

--- 8-1-graph.py
+++ 8-1-graph.py
@@ -9,7 +9,7 @@ time = np.array([i * 1 / frequency for i in range(data.size)])
 
 fig, ax = plt.subplots(figsize=(16, 10), dpi=500)
 
-ax.plot(time, data, lw = 1, c = "b", marker = "o", ms = 5, markevery = 300, label = "V(t)")
+ax.plot(time, data, lw = 1, c = "b", marker = "o", ms = 5, markerfacecolor = "violet", markevery = 300, label = "V(t)")
 #настройка легенды
 ax.legend(fontsize=10)
 
@@ -21,13 +21,13 @@ ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
-ax.grid(which='major', linestyle='-', linewidth=0.1, color = "silver")
-ax.grid(which='minor', linestyle = ':', color = "lightgrey", linewidth = 0.1)
+ax.grid(which='major', linestyle='-', linewidth=0.1, color = "grey")
+ax.grid(which='minor', linestyle = ':', color = "green", linewidth = 0.1)
 
 #настройка текста
 index = data.argmax()
-ax.text(6 * 10, 0.5, 'Время зарядки ' + str(time[index])[:6] + "c", fontsize = 10)
-ax.text(6 * 10, 0.7, 'Время разрядки ' + str(time[-1]-time[index])[:6] + "c", fontsize = 10)
+ax.text(6 * 10, 0.5, 'Время зарядки ' + str(time[index])[:4] + "c", fontsize = 10)
+ax.text(6 * 10, 0.7, 'Время разрядки ' + str(time[-1]-time[index])[:4] + "c", fontsize = 10)
 
 #настройка подписей осей и названия графика
 plt.xlabel('Напряжение, вольт', fontsize=10, bbox = {'pad': 0.1,'facecolor': 'white','edgecolor': 'white'})
